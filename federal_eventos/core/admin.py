efd8554ecc1fd768df3f5e0611a80a8ef29e9625
Lucas Oliveira <luk14236@gmail.com>
2021-09-15 20:31:23 -0300

    Primeiro commit contendo o Django, ambiente e inicio dos models.

---
+++ federal_eventos/core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Usuario, Ouvinte
+
+admin.site.register(Usuario)
+admin.site.register(Ouvinte)
---
+++ federal_eventos/core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=150)
+    tipo = models.CharField(max_length=50, default='Admin')
+    password = models.CharField(max_length=16)
+    email = models.CharField(max_length=250)
+    created = models.DatetimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+
+
+class Ouvinte(models.Model):
+    nome = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=10, blank=True)
+    password = models.CharField(max_length=16, blank=True)
+    confirmado = models.BooleanField(null=True)
+    created = models.DatetimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Ouvinte'
+        verbose_name_plural = 'Ouvintes'
