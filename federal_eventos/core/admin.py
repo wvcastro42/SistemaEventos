ce5c05546bf53670d38884f72dadce1d14129f5d
Weverton Castro <wvcastro42@gmail.com>
2021-09-23 02:40:30 -0300

    First version of all models

--- federal_eventos/core/admin.py
+++ federal_eventos/core/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Usuario, Ouvinte
+from .models import Palestrante, Usuario, Ouvinte, Evento, Atracao
 
-admin.site.register(Usuario)
-admin.site.register(Ouvinte)
+@admin.register(Usuario)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'created', 'updated')
+
+
+@admin.register(Ouvinte)
+class OuvinteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'created', 'updated')
+
+
+@admin.register(Palestrante)
+class PalestranteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'profissao', 'onde_trabalha')
+
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_inicial', 'data_final', 'created', 'updated')
+
+
+@admin.register(Atracao)
+class AtracaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'palestrante', 'atração', 'evento', 'data', 'local', 'created', 'updated') 
--- federal_eventos/core/models.py
+++ federal_eventos/core/models.py
@@ -6,7 +6,8 @@ class Usuario(models.Model):
     tipo = models.CharField(max_length=50, default='Admin')
     password = models.CharField(max_length=16)
     email = models.CharField(max_length=250)
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
@@ -14,14 +15,17 @@ class Usuario(models.Model):
     class Meta:
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
+        ordering = ("-created",)
 
 
 class Ouvinte(models.Model):
     nome = models.CharField(max_length=150)
-    cpf = models.CharField(max_length=10, blank=True)
+    cpf = models.CharField(max_length=11, blank=True)
     password = models.CharField(max_length=16, blank=True)
+    email = models.CharField(max_length=249, default=None)
     confirmado = models.BooleanField(null=True)
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
@@ -29,3 +33,65 @@ class Ouvinte(models.Model):
     class Meta:
         verbose_name = 'Ouvinte'
         verbose_name_plural = 'Ouvintes'
+        ordering = ("-created",)
+
+
+class Palestrante(models.Model):
+    nome = models.CharField(max_length=150, blank=False)
+    profissao = models.CharField(max_length=50, blank=False)
+    onde_trabalha = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Evento(models.Model):
+    nome = models.CharField(max_length=150, blank=False)
+    data_inicial = models.DateField(blank=True, null=True)
+    data_final = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # CRIAR HASH
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Evento'
+        verbose_name_plural = 'Eventos'
+        ordering = ("-created",)
+
+
+class Atracao(models.Model):
+    # CRIAR HASH
+    customer_types = (
+        (1,'Palestra'),
+        (2,'Workshop'),
+        (3,'Encontro'),
+        (4,'Seminário'),
+        (5,'Mesa-redonda'),
+        (6,'Simpósio'),
+        (7,'Painel'),
+        (8,'Fórum'),
+        (9,'Conferência'),
+        (10,'Jornada'),
+        (11,'Cursos'),
+        (12,'Colóquio'),
+        (13,'Semana'),
+    )
+    atração = models.IntegerField(choices=customer_types, default=1)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=150, blank=True)
+    data = models.DateField()
+    local = models.CharField(max_length=50, default='IFSP JCR')
+    palestrante = models.ForeignKey(Palestrante, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Atração'
+        verbose_name_plural = 'Atrações'
+        ordering = ("-created",)
